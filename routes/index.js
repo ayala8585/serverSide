6f1da7483008d43ef35e07931f5babc68c684e57
הינמן אילה <324218585@mbydomain>
2021-05-26 18:32:17 +0300

    ט"ו סיוון

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const jwt = require("jsonwebtoken");
+var urlToCreate = "mongodb://localhost:27017/substituteSystemDB";
+var url = "mongodb://localhost:27017/";
 
 const TOKEN_SECRET =
   "F9EACB0E0AB8102E999DF5E3808B215C028448E868333041026C481960EFC126";
@@ -9,15 +11,58 @@ const generateAccessToken = (username) => {
   return jwt.sign({username}, TOKEN_SECRET);
 };
 
+router.get("/createDB", (req, res) => {
+  MongoClient.connect(urlToCreate, function (err, db) {
+    console.log("err", err)
+    // if (err) {
+    //   console.error(err)
+    // } else {
+    //   console.log("Database created!");
+    //   db.close();
+    // }
+    res.send();
+  });
+})
+
+
+router.get("/createUserColection", () => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("projectDB");
+    dbo.createCollection("users", function (err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });
+})
+
+
 /* GET home page. */
-router.get('/login', function(req, res) {
-  console.log('here');
+router.get("/login", function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
   const { user, password } = req.query;
   //Check the pwd in the server
-  const token = generateAccessToken(user);
-  console.log("token", token);
-  return res.json({ token }).send();
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("projectDB");
+    var query = { user };
+    dbo.collection("users").find(query).toArray(function (err, result) {
+      if (err) throw err;
+      if (!result || result.length === 0) {
+        return res.status(401).send();
+      }
+      db.close();
+      if (result[0].password = password) {
+        const token = generateAccessToken(user);
+        console.log("token", token);
+        return res.json({ token }).send();
+      } else {
+        return res.status(401).send();
+      }
+    });
+  });
+
 });
 
 router.post("/signup", function (req, res) {
