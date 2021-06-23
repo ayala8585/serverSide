f6c82f61ab61d7a624b609f8e9ccdad7db0db275
הינמן אילה <324218585@mbydomain>
2021-06-23 15:22:28 +0300

    something important

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const db = require('./db')
+
+async function connect(){
+  await db.connect()
+}
+
+connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
