1d367f70e24908576db880330f53b6623a8da1ed
ayala8585 <83909904+ayala8585@users.noreply.github.com>
2021-11-01 17:13:08 +0200

    connect to new db

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ class db {
             useUnifiedTopology: true
         }
 
-        let url = "mongodb+srv://Mali:Mali@cluster0.blxgk.mongodb.net/Project1?retryWrites=true&w=majority";
+        let url = "mongodb+srv://Ayala:aylhe@substitutesystem.lwf0k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
         mongoose.connect(url, connectionParams).then((connection) => {
             autoIncrement.initialize(connection);
 
