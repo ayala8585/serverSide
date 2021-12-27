28c5297cae30c6a199451b595426f7d6b86fa93a
malka-farkash <m81653@gmail.com>
2021-12-27 18:10:29 +0200

    delete files

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors= require("cors");
 
@@ -23,20 +22,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/teacher",teacherRouter);
 
-// app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
