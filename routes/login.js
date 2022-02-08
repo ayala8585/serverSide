c8022568faf1d3fbd13f473a053703aea74e8874
malka-farkash <m81653@gmail.com>
2022-02-08 17:05:04 +0200

    fixes due to client side

--- routes/login.js
+++ routes/login.js
@@ -3,14 +3,20 @@ const Contact = require("../models/contact");
 const contact = require("../models/contact");
 
 const login = async (req, res) => {
-  const { id, password } = req.body;
+  const { id, password } = req.query;
+
   try {
-    const concat = await Contact.findOne({ personId: id, password });
+    const contact = await Contact.findOne({ personId: id, password });
     if (!contact) return res.status(404).send("User does not exit");
-    const token = jwt.sign(contact, process.env.TOKEN_SECRET, {
-      expiresIn: "24h",
-    });
-    return res.json({ token });
+    const token = jwt.sign(
+      { id: contact.personId, password: contact.password },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return res.json({ token, contact });
   } catch (error) {
     console.log("error finding contact", error);
     return res.status(400).send("Can't find user");
